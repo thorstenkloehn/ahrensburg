e771bc0d3dd96243ceaed2cb9e5dddad11038fda
Thorsten Klöhn <thorstenkloehn@gmail.com>
2025-10-10 17:24:09 +0200

    start

---
+++ ahrensburg.city/Program.cs
@@ -0,0 +1,28 @@
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services
+    .AddOrchardCms()
+    // // Orchard Specific Pipeline
+    // .ConfigureServices( services => {
+    // })
+    // .Configure( (app, routes, services) => {
+    // })
+;
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseOrchardCore();
+
+app.Run();
